675fd48dc70ba1285bdb000e658e12ca15752ad5
孟麟伦 <menglinl@chanjet.com>
2020-08-12 23:41:22 +0800

    cloudFunctionInvoke

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,25 +1,55 @@
-import React, { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import { AtButton,AtFab } from 'taro-ui'
-
+import React, { Component } from 'react';
+import { View, Text } from '@tarojs/components';
+import { AtButton,AtFab,AtRate } from 'taro-ui';
 import "taro-ui/dist/style/components/button.scss" // 按需引入
-import './index.less'
+import "taro-ui/dist/style/components/rate.scss";
+import "taro-ui/dist/style/components/icon.scss";
+import './index.less';
+import Taro from '@tarojs/taro';
 
-export default class Index extends Component {
+interface IndexState {
 
+  value: number;
+
+}
+export default class Index extends Component<any,IndexState> {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: 2
+    }
+  }
+  handleChange (value) {
+    this.setState({
+      value
+    })
+  }
   componentWillMount () { }
 
   componentDidMount () {
-    wx.cloud.init();
+    wx.cloud.init({
+      // traceUser:true,
+    });
     const db = wx.cloud.database();
     const suggestions = db.collection('suggestions');
     suggestions.get().then(res => {
       // res.data 包含该记录的数据
       console.log(res.data)
-    })
-
+    });
+    // console.log(wx);
+    this.getLogin()
   }
-
+  getLogin () {
+    Taro.cloud
+        .callFunction({
+            name: "prod",
+            data: {}
+        })
+        .then(res => {
+            const context1 = JSON.stringify(res.result);
+            console.log(context1)
+        })
+}
   componentWillUnmount () { }
 
   componentDidShow () { }
@@ -33,7 +63,11 @@ export default class Index extends Component {
         <AtButton type='primary'>I need Taro UI</AtButton>
         <Text>Taro UI 支持 Vue 了吗？</Text>
         <AtButton type='primary' circle={true}>支持</AtButton>
-        <Text>共建？</Text>
+        <AtRate
+        size={30}
+        value={this.state.value}
+        onChange={this.handleChange.bind(this)}
+      />
       </View>
     )
   }
